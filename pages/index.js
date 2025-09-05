b1148537e9acd41ac89871971b5d209847f2123f
Larissa Abreu <35471939+larita1978@users.noreply.github.com>
2025-09-05 02:41:08 +0000

    Configurado o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Eduardo, se você me ama da uma risadinha! </h1>
+function Home() {
+  return <h1>Eduardo, se você me ama da uma risadinha! </h1>;
 }
 
 export default Home;
